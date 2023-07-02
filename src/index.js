5efbb532b10e3e16f0fa90fe4e21ab6683b54110
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2023-07-02 17:31:37 +0300

    change example file to header

---
+++ src/index.js
@@ -0,0 +1,41 @@
+[
+  {
+    cook: {
+      name: 'Heston Chan',
+      imgUrl: 'https://tasty-treats-backend.p.goit.global/HestonChan.png',
+    },
+    topic: {
+      name: 'Grilled, smoky, delicious barbecue',
+      area: 'Spanish',
+      previewUrl: 'https://tasty-treats-backend.p.goit.global/previewDish1.png',
+      imgUrl: 'https://tasty-treats-backend.p.goit.global/imgDish1.png',
+    },
+    _id: '647495d0c825f1570b04182d',
+  },
+  {
+    cook: {
+      name: 'Ana Smyth',
+      imgUrl: 'https://tasty-treats-backend.p.goit.global/AnaSmyth.png',
+    },
+    topic: {
+      name: 'Hot, cheesy and satisfying pizza',
+      area: 'Italian',
+      previewUrl: 'https://tasty-treats-backend.p.goit.global/previewDish2.png',
+      imgUrl: 'https://tasty-treats-backend.p.goit.global/imgDish2.png',
+    },
+    _id: '647495d0c825f1570b04182e',
+  },
+  {
+    cook: {
+      name: 'Ferran Meyer',
+      imgUrl: 'https://tasty-treats-backend.p.goit.global/FerranMeyer.png',
+    },
+    topic: {
+      name: 'Fluffy and sweet pancakes',
+      area: 'American',
+      previewUrl: 'https://tasty-treats-backend.p.goit.global/previewDish3.png',
+      imgUrl: 'https://tasty-treats-backend.p.goit.global/imgDish3.png',
+    },
+    _id: '647495d0c825f1570b04182f',
+  },
+];
